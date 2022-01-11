353d62de1f6b7e9181bc6cc22ac81ea4f2dfae16
huy8856 <thai.giahuy123@gmail.com>
2022-01-11 20:52:29 +0700

    Count down toggle start stop button practice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import Content from "./Content"
 function App() {
+	const [count, setCount] = useState(60)
+
+	const timerId = useRef()
+	const prevCount = useRef()
+	const h1Ref = useRef()
+
+	useEffect(() => {
+		prevCount.current = count
+	}, [count])
+
+	useEffect(() => {
+		console.log(h1Ref.current)
+	})
+
+	const handleStart = () => {
+		timerId.current = setInterval(() => {
+			setCount((prevCount) => prevCount - 1)
+		}, 1000)
+	}
+
+	const handleStop = () => {
+		clearInterval(timerId.current)
+	}
 	return (
 		<div style={{ padding: 32 }}>
 			<Content />
+			<h1 ref={h1Ref}>{count}</h1>
+			<button onClick={handleStart}>Start</button>
+			<button onClick={handleStop}>Stop</button>
 		</div>
 	)
 }
--- src/Content.js
+++ src/Content.js
@@ -1,48 +1,7 @@
 import { useEffect, useState } from "react"
 
-const lessons = [
-	{
-		id: 1,
-		name: "React là gì?",
-	},
-	{
-		id: 2,
-		name: "SPA/MPA",
-	},
-	{
-		id: 3,
-		name: "Function là gì",
-	},
-]
-
 function Content() {
-	const [lessonId, setLessonId] = useState(1)
-
-	useEffect(() => {
-		const handleEvent = ({ detail }) => {
-			console.log({ detail })
-		}
-		window.addEventListener(`lesson-${lessonId}`, handleEvent)
-		return () => {
-			window.removeEventListener(`lesson-${lessonId}`, handleEvent)
-		}
-	}, [lessonId])
-
-	return (
-		<div>
-			<ul>
-				{lessons.map((lesson) => (
-					<li
-						key={lesson.id}
-						style={{ color: lessonId === lesson.id ? "red" : "#333" }}
-						onClick={() => setLessonId(lesson.id)}
-					>
-						{lesson.name}
-					</li>
-				))}
-			</ul>
-		</div>
-	)
+	return <div></div>
 }
 
 export default Content
