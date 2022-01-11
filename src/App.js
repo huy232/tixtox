d22abb8b26bdb26050787055a4e71ebeda7b24b1
huy8856 <thai.giahuy123@gmail.com>
2022-01-11 19:34:09 +0700

    Real time render chat prototype logic practice

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
 import { useState } from "react"
-
+import Content from "./Content"
 function App() {
-	const [job, setJob] = useState("")
-	const [jobs, setJobs] = useState(() => {
-		const storageJobs = JSON.parse(localStorage.getItem("jobs"))
-		return storageJobs
-	})
-
-	const handleSubmit = () => {
-		setJobs((prev) => {
-			const newJobs = [...prev, job]
-
-			const jsonJobs = JSON.stringify(newJobs)
-			localStorage.setItem("jobs", jsonJobs)
-
-			return newJobs
-		})
-		setJob("")
-	}
-
 	return (
 		<div style={{ padding: 32 }}>
-			<input type="text" value={job} onChange={(e) => setJob(e.target.value)} />
-			<button onClick={handleSubmit}>Add</button>
-			<ul>
-				{jobs.map((job, i) => (
-					<li key={i}>{job}</li>
-				))}
-			</ul>
+			<Content />
 		</div>
 	)
 }
